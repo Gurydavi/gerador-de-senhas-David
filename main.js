55585c9b039c50b5643eec7772a3ebf2e07f5478
Gurydavi <david.borges.silva@escola.pr.gov.br>
2025-07-01 10:51:07 +0000

    aumento

--- main.js
+++ main.js
@@ -7,7 +7,28 @@ const botoes = document.querySelectorAll('.parametro-senha__botao');
 console.log(botoes)
 
 botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
 
 function diminuiTamanho(){
     tamanhoSenha = tamanhoSenha-1;
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+function aumentaTamanho(){
+    tamanhoSenha = tamanhoSenha+1;
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+function diminuiTamanho(){
+    if (tamanhoSenha > 1){
+        tamanhoSenha = tamanhoSenha-1;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+function aumentaTamanho(){
+    if (tamanhoSenha < 20){
+       tamanhoSenha = tamanhoSenha+1;
+    }
+    numeroSenha.textContent = tamanhoSenha;
 }
